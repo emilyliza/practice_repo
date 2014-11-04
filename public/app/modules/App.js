618a624482a24d1413724b7bd926d039a1d20cfa
Justin Shreve <justin@infoshreve.com>
2014-11-04 13:27:06 -0800

    new App file

---
+++ public/app/modules/App.js
@@ -0,0 +1,35 @@
+(function() {
+
+	angular.module('app', ['ui.router'])
+	
+	.directive('nav', function() {
+		return {
+			restrict: 'E',
+			controller: 'ApplicationController',
+			controllerAs: 'appCtrl',
+			templateUrl: '/app/templates/nav.html'
+		};
+	})
+
+	.controller('ApplicationController', function ($scope, $rootScope, USER_ROLES, AUTH_EVENTS, AuthService, $state) {
+		$scope.currentUser = null;
+		$scope.authChecked = false;
+		$scope.userRoles = USER_ROLES;
+		$scope.isAuthorized = AuthService.isAuthorized;
+		$scope.$state = $state;	// for navigation active to work
+
+		$scope.setCurrentUser = function (user) {
+			$scope.currentUser = user;
+		}
+
+		AuthService.check().then(function (user) {
+			$scope.setCurrentUser(user);
+			$scope.authChecked = true;
+		}, function(res) {
+			console.log('Not logged in.');
+		});
+
+    });
+
+
+})();
