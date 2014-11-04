04919c39c734d11a7a6acc780275273adefeeb3f
Justin Shreve <justin@infoshreve.com>
2014-11-04 13:44:18 -0800

    first protractor test

---
+++ public/app/tests/home_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('jgsApp.login module', function() {
+
+	beforeEach(module('jgsApp.login'));
+
+	describe('login controller', function(){
+
+		it('should ....', inject(function($controller) {
+			//spec body
+			var loginCtrl = $controller('LoginController');
+			expect(loginCtrl).toBeDefined();
+		}));
+
+	});
+});
