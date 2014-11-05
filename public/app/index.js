31ee7028e8d140157f2eecfc2ed1e1fb9d60f9ee
Justin Shreve <justin@infoshreve.com>
2014-11-04 16:33:52 -0800

    moved Login constants to Login module

--- public/app/index.js
+++ public/app/index.js
@@ -12,22 +12,6 @@
 		"account"
 	])
 
-	.constant('AUTH_EVENTS', {
-		loginSuccess: 'auth-login-success',
-		loginFailed: 'auth-login-failed',
-		logoutSuccess: 'auth-logout-success',
-		sessionTimeout: 'auth-session-timeout',
-		notAuthenticated: 'auth-not-authenticated',
-		notAuthorized: 'auth-not-authorized'
-	})
-
-	.constant('USER_ROLES', {
-		all: '*',
-		admin: 'admin',
-		editor: 'editor',
-		guest: 'guest'
-	})
-
 	.config(function( $locationProvider, $urlRouterProvider) {
 		$locationProvider.html5Mode(true).hashPrefix('!');
 	})
