2bd52323f3ee01722b84399ce34d5a02948a5977
Justin Shreve <justin@infoshreve.com>
2014-12-23 14:46:21 -0800

    auth integration, chargeback api
    chargebacks api endpoint now queries by merchant.

--- python/server.py
+++ python/server.py
@@ -17,6 +17,10 @@ from tornado.log import enable_pretty_logging
 from tornado.options import define, options
 import jwt
 import functools
+import requests
+from src.clients.client_register import ClientServiceReg
+from src.clients.client_serviceauth import ClientServiceAuth
+from src.clients.client_serviceaas import ClientServiceAAS
 
 
 define("port", default=8888, help="run on the given port", type=int)
@@ -32,7 +36,7 @@ def authenticated(method):
         except KeyError:
             token = None    
 
-        print token
+        #print token
         if token is not None:
             try:
                 self.user = jwt.decode(token, os.environ['TOKEN_SECRET'])
@@ -45,6 +49,40 @@ def authenticated(method):
     return wrapper
 
 
+
+
+class UserAuthentication:
+
+    def __init__(self):
+        # contact the register service to get other services URLs
+        cs_reg = ClientServiceReg( os.environ['AUTH_SERVICE_HOST'] )
+        print "Getting service list..."
+        errno, self.services_dd = cs_reg.find_services_all()
+        if errno != 0:
+            raise Exception("Unable to contact the Register service")
+        self._authenticate_service()
+
+    def _authenticate_service(self):
+        # get auth token with data service name and password
+        cs_auth = ClientServiceAuth(self.services_dd['ServiceAuth'])
+        print "Service Authentication..."
+        errno, auth_token = cs_auth.login( os.environ['AUTH_SERVICE_NAME'], os.environ['AUTH_SERVICE_PASS'] )
+        if errno != 0:
+            raise Exception("Data service unable to log in to the Auth Service")
+        # get service token for accessing the Authentication, Authorization, and Sessioning (AAS) service
+        print "Service Authentication Login..."
+        errno, self.service_token = cs_auth.service_login(auth_token, 'ServiceAAS')
+        if errno != 0:
+            raise Exception("Unable to obtain a service token for 'ServiceAAS'")
+        self.claas = ClientServiceAAS(self.services_dd['ServiceAAS'])
+
+    def user_login(self, username, password):
+        errno, ret_dd = self.claas.user_login(self.service_token, username, password)
+        return errno, ret_dd
+
+    def user_perms(self, user_token):
+        errno, perms = self.claas.get_user_permission(self.service_token, user_token)
+        return errno, perms
 
 
 
@@ -98,25 +136,61 @@ class BaseHandler(tornado.web.RequestHandler):
 class HomeHandler(BaseHandler):
     def get(self):
         if os.environ['ENV'] == "production":
+            # production -- will load just index.html, all other assets in this file are in CDN
             self.render("../dist/index.html")
         else:
+            # dev - will load this file and all local js/less files. slower, but easier to work
             self.render("../public/index.html")
 
 
 
 class LoginHandler(BaseHandler):
+    @tornado.web.asynchronous
     def post(self):
         
         post = tornado.escape.json_decode(self.request.body)
         
-        if post['email'] != "test@chargeback.com" and post['password'] != "test":
+        uauth = UserAuthentication()
+        error_num, user_session_data = uauth.user_login(post['username'], post['password'])
+        
+        if error_num != 0:
             self.set_status(401)
             self.finish({ 'errors': { 'password': 'invalid password' }})
         else:
+            # print user_session_data
+
+            # get merchant data via permissions
+            error_num, user_perm_data = uauth.user_perms(user_session_data['user_token'])
+            #print user_perm_data
+
+            merchants = {}
+            merchant_checker = []
+            for k,v in user_perm_data['merchants'].iteritems():
+                if (v['MerchID'] not in merchant_checker):
+                    m = {
+                        "id": v['MerchID'],
+                        "name": v['MerchName'],
+                        "alt_name": v['MerchAltname'],
+                        "mids": [{
+                            "mid": k,
+                            "mid_display": v['MID']
+                        }]
+                    }
+                    merchants[ str(v['MerchID']) ] = m
+                    merchant_checker.append(v['MerchID'])
+                else:
+                    merchants[ str(v['MerchID']) ]['mids'].append({
+                        "mid": k,
+                        "mid_display": v['MID']
+                    })
+            
+
             # We are sending the profile inside the token
             user = {
-                'fullname': 'test merchant',
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)
+                'fullname': post['username'],
+                'user_token': user_session_data['user_token'],
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=3600),
+                "merchants": merchants
             }
             token = jwt.encode(user, os.environ['TOKEN_SECRET'])
             del user['exp']   # don't want to include this
@@ -146,53 +220,87 @@ class ChargebacksHandler(BaseHandler):
         merchant = self.get_argument('merchant', None)
         status = self.get_argument('status', None)
         card_type = self.get_argument('card_type', None)
-        query = self.get_argument('query', None)
+        query = self.get_argument('query', "")
+        
         search = {}
+        search['$and'] = []
 
+        # if mid, then make sure it belongs to user
         if (mid is not None): 
-            search['DocGenData.portal_data.MidNumber'] = str(mid)
+            p = 0
+            for k,merch in self.user['merchants'].iteritems():
+                for m in merch['mids']:
+                    if m['mid'] == str(mid):
+                        p = 1
+            
+            if p == 1:
+                search['$and'].append( { 'DocGenData.portal_data.MidNumber': str(mid) })
+            else:
+                # if it doesn't belong to user, just give them back all stuff they own
+                merchant_query = []
+                for k,merch in self.user['merchants'].iteritems():
+                    for m in merch['mids']:
+                        merchant_query.append( { 'DocGenData.portal_data.MidNumber': m['mid'] } )
+                search['$and'].append( { '$or': merchant_query } )
 
-        if (merchant is not None): 
-            search['DocGenData.derived_data.Merchant'] = str(merchant)
+        if mid is None: 
+            merchant_query = []
+            for k,merch in self.user['merchants'].iteritems():
+                for m in merch['mids']:
+                    merchant_query.append( { 'DocGenData.portal_data.MidNumber': m['mid'] } )
+
+            merchant_query.append( { 'DocGenData.portal_data.MidNumber': "272336" } )
+            search['$and'].append( { '$or': merchant_query } )
+
+
+        
+
+        # if (merchant is not None): 
+        #     search['$and'].append( { 'DocGenData.derived_data.Merchant'] = str(merchant) })
 
         if (start is not None): 
             start_date = datetime.datetime.fromtimestamp(float(start)/1000)
-            search['DocGenData.portal_data.ChargebackDate'] = { '$gte': start_date.strftime("%Y-%m-%d") }
+            search['$and'].append( { 'DocGenData.portal_data.ChargebackDate': { '$gte': start_date.strftime("%Y-%m-%d") } })
 
         if (end is not None): 
             end_date = datetime.datetime.fromtimestamp(float(end))
-            search['DocGenData.portal_data.ChargebackDate'] = { '$gte': end_date.strftime("%Y-%m-%d") }
+            search['$and'].append( { 'DocGenData.portal_data.ChargebackDate': { '$gte': end_date.strftime("%Y-%m-%d") } })
 
         #if (status is not None): 
         #    search['DocGenData.derived_data.Status'] = str(status)
 
 
         if (card_type is not None): 
-            search['DocGenData.gateway_data.CcType'] = str(card_type)
+            search['$and'].append( {'DocGenData.gateway_data.CcType': str(card_type) })
 
-        if query is not None:
+        if query:
             if query.isnumeric():
-	            search['$or'] = [
+	            search['$and'].append( { '$or': [
                     { 'DocGenData.portal_data.ChargebackAmt': float(query) },
                     { 'DocGenData.derived_data.uuid': int(query) }
-                ];
+                ]})
             else:
                 pattern = re.compile('.*'+query+'.*', re.IGNORECASE)
                 print pattern
-                search['$or'] = [
+                search['$and'].append( { '$or': [
                 	#{ 'DocGenData.derived_data.status.name': pattern },
                 	{ 'DocGenData.gateway_data.FirstName': pattern },
                 	{ 'DocGenData.gateway_data.LastName': pattern },
                     { 'DocGenData.derived_data.MerchantName': pattern }
-                ]
+                ]})
         
+        
+        
+                
+
+
         print ' ==> Query: ' + str(search)
         print ' ==> Query sorting: ' + sort + ' ' + str(sort_dir)
 		
         # TODO: need index on sortable fields!!
         #self.db.dispute.ensure_index(sort)
-        #cursor = self.db.dispute.find(search).sort([(sort, sort_dir)]).skip(skip)
-        cursor = self.db.dispute.find(search).skip(skip)
+        cursor = self.db.dispute.find(search).sort([(sort, sort_dir)]).skip(skip)
+        #cursor = self.db.dispute.find(search).skip(skip)
         
         out = []
         data = yield cursor.to_list(int(limit))
@@ -291,16 +399,19 @@ def cleanData(cb):
 	# clean data a little
     clean = {}
     
-    dt = datetime.datetime.strptime( str(cb['DocGenData']['portal_data']['RequestDate']), "%Y-%m-%d %H:%M:%S")
-
-    clean['__id'] = str(cb['_id'])
     clean['shipping_data'] = cb['DocGenData']['shipping_data']
     clean['gateway_data'] = cb['DocGenData']['gateway_data']
     clean['crm_data'] = cb['DocGenData']['crm_data']
     clean['portal_data'] = cb['DocGenData']['portal_data']
     clean['derived_data'] = cb['DocGenData']['derived_data']
+
+    if 'RequestDate' in cb['DocGenData']['portal_data'].keys():
+        if str(cb['DocGenData']['portal_data']['RequestDate']):
+            dt = datetime.datetime.strptime( str(cb['DocGenData']['portal_data']['RequestDate']), "%Y-%m-%d %H:%M:%S")
+            clean['portal_data']['RequestDate'] = str(dt.isoformat())
+
+    clean['__id'] = str(cb['_id'])
     
-    clean['portal_data']['RequestDate'] = str(dt.isoformat())
     clean['shipping_data']['ShippingDate'] = str(cb['DocGenData']['shipping_data']['ShippingDate'])
     clean['gateway_data']['TransDate'] = str(cb['DocGenData']['gateway_data']['TransDate'])
     clean['crm_data']['OrderDate'] = str(cb['DocGenData']['crm_data']['OrderDate'])
