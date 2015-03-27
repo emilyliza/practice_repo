0a0b793c58e831665637ffe8b0a796cc5260abc8
Justin Shreve <justin@infoshreve.com>
2015-03-26 17:15:37 -0700

    changes to config

--- postNpmInstall.js
+++ postNpmInstall.js
@@ -10,13 +10,4 @@ if (env !== 'production') {
 		return process.exit(0);
 	});
     
-} else if (env == "production") {
-
-	// use "n" to install and use proper node version.
-	exec("npm install -g n && n 0.12.0 && n use 0.12.0", function(err, stdout, stderr) {
-		console.log(stdout);
-		console.log(stderr);	
-		return process.exit(0);
-	});
-
 }
