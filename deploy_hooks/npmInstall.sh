ec5e4607d96acb0225fb18af9675774b58ef61d0
Justin Shreve <justin@infoshreve.com>
2015-04-02 11:17:13 -0700

    pipe to file [skip ci]

--- deploy_hooks/npmInstall.sh
+++ deploy_hooks/npmInstall.sh
@@ -6,4 +6,6 @@ echo 'running mkdir'
 mkdir $DIRECTORY
 
 echo 'Running npm install: npm --prefix /var/www/cart install'
-cd /var/www/cart && npm install --production --unsafe-perm
+cd /var/www/cart && npm install --production --unsafe-perm 2> /var/www/npm_install.txt
+
+exit 0;
