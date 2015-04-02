6f1ce3e8766954f89f761ea1e703ce6af5f164f1
Justin Shreve <justin@infoshreve.com>
2015-04-02 14:25:23 -0700

    s3 deploy updates and aws copy

--- deploy_hooks/npmInstall.sh
+++ deploy_hooks/npmInstall.sh
@@ -6,4 +6,7 @@ cd /var/www/cart && npm prune 2> /var/www/npm_install.txt
 echo 'Running npm install: npm install --production'
 cd /var/www/cart && npm install --production 2> /var/www/npm_install.txt
 
+echo 'Copying build artifacts'
+aws s3 cp s3://chargeback-builds/dist /var/www/cart/dist --recursive
+
 exit 0;
