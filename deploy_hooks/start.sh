4d0d681be8fb260842424caa53173f32f3d255cb
Justin Shreve <justin@infoshreve.com>
2015-04-02 10:50:05 -0700

    start server with env and use root for port 80

--- deploy_hooks/start.sh
+++ deploy_hooks/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'Starting forever: /var/www/cart/node_modules/forever/bin/forever /var/www/cart/server.js'
-/var/www/cart/node_modules/forever/bin/forever /var/www/cart/server.js
+env $(cat /etc/environment) /var/www/cart/node_modules/forever/bin/forever /var/www/cart/server.js
 
 exit 0;
