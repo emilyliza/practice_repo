04bac9e2ed032b4a00348418b40f1b9d8d867e7f
Justin Shreve <justin@infoshreve.com>
2015-04-02 13:43:46 -0700

    testing travis s3 deploy

--- deploy_hooks/start.sh
+++ deploy_hooks/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /var/www/cart && node_modules/forever/bin/forever server.js -o /var/www/node.log;
+cd /var/www/cart && node_modules/forever/bin/forever start server.js -o /var/www/node.log;
