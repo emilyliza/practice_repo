82bacef2ba8316e8fdac01e5ea5731b6562dc763
Justin Shreve <justin@infoshreve.com>
2015-04-02 10:55:29 -0700

    more deploy permissions stuff [skip ci]

--- deploy_hooks/npmInstall.sh
+++ deploy_hooks/npmInstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+DIRECTORY=/var/www/cart/node_modules
+
+echo 'running mkdir'
+mkdir $DIRECTORY
+
 echo 'Running npm install: npm --prefix /var/www/cart install'
 npm --prefix /var/www/cart install --production
--- deploy_hooks/rootStuff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIRECTORY=/var/www/cart/node_modules
-
-echo 'running mkdir'
-mkdir $DIRECTORY
-echo 'running chown to node_modules'
-chown justin $DIRECTORY
