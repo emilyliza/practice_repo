a73bc2e1b517a25c77b5c11206dc93c134fc213f
Tom Jensen <tom.jensen@chargeback.com>
2015-07-27 15:25:23 -0600

    Added back in changes to fix the differneces btween master and dev branches.

--- models/User.js
+++ models/User.js
@@ -19,6 +19,7 @@ module.exports = function(app) {
 		'password': { type: String, set: Util.hash_password },
 		'active': { type: Boolean, default: true },
 		'admin': { type: Boolean, default: false },
+		'license': {type: String},
 		'send_to': {
 			'email': { type: String },
 			'fax': { type: String }
